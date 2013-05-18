c623f9fa2ff4c033d70d5910bbac365b8b46ac64
Emil Pirfalt <emil.pirfalt@gmail.com>
2013-05-18 18:02:22 +0200

    Removed sequence from index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 exports.compose = require('./compose');
-exports.sequence = require('./sequence');
 exports.partial = require('./partial');
 exports.partialRight = require('./partialright');
 exports.flipArgs = require('./flipargs');
