ac8f7f12cead3d3aa9f11a0bedd10dd50e0bddaa
Emil Pirfalt <emil.pirfalt@gmail.com>
2013-05-21 10:03:43 +0200

    Index update

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 exports.compose = require('./compose');
 exports.partial = require('./partial');
 exports.partialRight = require('./partialright');
-exports.flipArgs = require('./flipargs');
+exports.flipArgs = require('./curry');
+exports.curry = require('./flipargs');
+exports.unary = require('./unary');
