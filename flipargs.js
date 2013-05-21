d0844b932de9862dafe8fbe609539344dc7e4a9f
Emil Pirfalt <emil.pirfalt@gmail.com>
2013-05-21 10:00:36 +0200

    FlipArgs now use sliced

--- flipargs.js
+++ flipargs.js
@@ -1,7 +1,11 @@
-//  flipArgs(f) -> (a,b,c) -> f(c,b,a)
-var flipArgs = module.exports = function(f) {
+//  flipArgs(f:(a,b,c)) => f(c,b,a)
+var sliced = require('sliced');
+
+module.exports = flipArgs
+
+function flipArgs(f) {
   return function() {
-    var args = [].slice.call(arguments);
+    var args = sliced(arguments);
     return f.apply(this, args.reverse());
   };
-};
+}
