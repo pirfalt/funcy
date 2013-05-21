6e76d70e91a7bf7262ab70619a1bb4672e6a2e74
Emil Pirfalt <emil.pirfalt@gmail.com>
2013-05-21 09:58:14 +0200

    Partial fix
    Partial now uses sliced over [].slice
    Fixed bug at 4 newArgs.
    Added optimized 3 oldArgs.

--- partial.js
+++ partial.js
@@ -1,6 +1,6 @@
 // partial(fn, old1, old2)(new)  =>  fn(old1, old2, new)
 // optimized up until fn(old1,odl2,old3,new1,new2)
-var __slice = Array.prototype.slice;
+var sliced = require('sliced');
 
 module.exports = partial;
 
@@ -22,8 +22,8 @@ function partial(func /* ...partialArgs */) {
     : (newLength === 1) ? func(o[1], n[0])
     : (newLength === 2) ? func(o[1], n[0], n[1])
     : (newLength === 3) ? func(o[1], n[0], n[1], n[2])
-    : (newLength === 3) ? func(o[1], n[0], n[1], n[2], n[3])
-    : func.apply(this, __slice.call(o, 1).concat(__slice.call(n)))
+    : (newLength === 4) ? func(o[1], n[0], n[1], n[2], n[3])
+    : func.apply(this, sliced(o, 1).concat(sliced(n)))
   }
 
   // 2 arg: f -> func(arg1, arg2, ...newArgs)
@@ -34,12 +34,24 @@ function partial(func /* ...partialArgs */) {
     : (newLength === 1) ? func(o[1], o[2], n[0])
     : (newLength === 2) ? func(o[1], o[2], n[0], n[1])
     : (newLength === 3) ? func(o[1], o[2], n[0], n[1], n[2])
-    : (newLength === 3) ? func(o[1], o[2], n[0], n[1], n[2], n[3])
-    : func.apply(this, __slice.call(o, 1).concat(__slice.call(n)))
+    : (newLength === 4) ? func(o[1], o[2], n[0], n[1], n[2], n[3])
+    : func.apply(this, sliced(o, 1).concat(sliced(n)))
   }
 
-  // 3 or more args: f -> func(...oldArgs, ...newArgs)
-  : (o = __slice.call(o, 1), function() {
-    return func.apply(this, o.concat(__slice.call(arguments)))
+  // 3 arg: f -> func(arg1, arg2, ...newArgs)
+  : (oldLength === 4) ? function() {
+    var n = arguments;
+    var newLength = arguments.length;
+    return (newLength === 0) ? func(o[1], o[2], o[3])
+    : (newLength === 1) ? func(o[1], o[2], o[3], n[0])
+    : (newLength === 2) ? func(o[1], o[2], o[3], n[0], n[1])
+    : (newLength === 3) ? func(o[1], o[2], o[3], n[0], n[1], n[2])
+    : (newLength === 4) ? func(o[1], o[2], o[3], n[0], n[1], n[2], n[3])
+    : func.apply(this, sliced(o, 1).concat(sliced(n)))
+  }
+
+  // 4 or more args: f -> func(...oldArgs, ...newArgs)
+  : (o = sliced(o, 1), function() {
+    return func.apply(this, o.concat(sliced(arguments)))
   })
 }
--- partialright.js
+++ partialright.js
@@ -1,6 +1,6 @@
 // partial(fn, old1, old2)(new)  =>  fn(old1, old2, new)
 // optimized up until fn(old1,odl2,old3,new1,new2)
-var __slice = Array.prototype.slice;
+var sliced = require('sliced');
 
 module.exports = partialRight;
 
@@ -22,8 +22,8 @@ function partialRight(func /* ...partialArgs */) {
     : (newLength === 1) ? func(n[0], o[1])
     : (newLength === 2) ? func(n[0], n[1], o[1])
     : (newLength === 3) ? func(n[0], n[1], n[2], o[1])
-    : (newLength === 3) ? func(n[0], n[1], n[2], n[3], o[1])
-    : func.apply(this, __slice.call(n).concat(__slice.call(o, 1)))
+    : (newLength === 4) ? func(n[0], n[1], n[2], n[3], o[1])
+    : func.apply(this, sliced(n).concat(sliced(o, 1)))
   }
 
   // 2 arg: f -> func(arg1, arg2, ...newArgs)
@@ -34,12 +34,24 @@ function partialRight(func /* ...partialArgs */) {
     : (newLength === 1) ? func(n[0], o[1], o[2])
     : (newLength === 2) ? func(n[0], n[1], o[1], o[2])
     : (newLength === 3) ? func(n[0], n[1], n[2], o[1], o[2])
-    : (newLength === 3) ? func(n[0], n[1], n[2], n[3], o[1], o[2])
-    : func.apply(this, __slice.call(n).concat(__slice.call(o, 1)))
+    : (newLength === 4) ? func(n[0], n[1], n[2], n[3], o[1], o[2])
+    : func.apply(this, sliced(n).concat(sliced(o, 1)))
   }
 
-  // 3 or more args: f -> func(...oldArgs, ...newArgs)
-  : (o = __slice.call(o, 1), function() {
-    return func.apply(this, __slice.call(arguments).concat(o));
+  // 3 arg: f -> func(arg1, arg2, ...newArgs)
+  : (oldLength === 4) ? function() {
+    var n = arguments;
+    var newLength = arguments.length;
+    return (newLength === 0) ? func(o[1], o[2], o[3])
+    : (newLength === 1) ? func(n[0], o[1], o[2], o[3])
+    : (newLength === 2) ? func(n[0], n[1], o[1], o[2], o[3])
+    : (newLength === 3) ? func(n[0], n[1], n[2], o[1], o[2], o[3])
+    : (newLength === 4) ? func(n[0], n[1], n[2], n[3], o[1], o[2], o[3])
+    : func.apply(this, sliced(n).concat(sliced(o, 1)))
+  }
+
+  // 4 or more args: f -> func(...oldArgs, ...newArgs)
+  : (o = sliced(o, 1), function() {
+    return func.apply(this, sliced(arguments).concat(o));
   })
 }
